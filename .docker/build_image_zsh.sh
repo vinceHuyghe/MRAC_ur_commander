058951acde69263e0f11e09505a7b354fa068cd8
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2023-10-03 10:15:55 +0200

    bash build

--- .docker/build_image_zsh.sh
+++ .docker/build_image_zsh.sh
@@ -5,5 +5,5 @@ echo -e "Building iaac_ur_commander:lastest image"
 DOCKER_BUILDKIT=1 \
 docker build --pull --rm -f ./.docker/Dockerfile \
 --build-arg BUILDKIT_INLINE_CACHE=1 \
---target bash \
+--target dev \
 --tag iaac_ur_commander:latest .
